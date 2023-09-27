07cef292c08e9ab9c5693b0125fa5a0061d34206
Siriusmart <71584876+Siriusmart@users.noreply.github.com>
2023-09-27 20:01:57 +0100

    tree diff completed

--- src/structs/diff.rs
+++ src/structs/diff.rs
@@ -2,33 +2,57 @@ use std::path::{Path, PathBuf};
 
 use goodmorning_bindings::services::v1::{V1DirTreeItem, V1DirTreeNode};
 
-#[derive(Debug, PartialEq, Eq)]
+use crate::functions::DEFAULT_VIS;
+
+const DIR_SIZE: u64 = 0;
+
+#[derive(Debug, PartialEq, Eq, Default)]
 pub struct TreeDiff {
-    pub deletes: Vec<PathBuf>,
+    pub created: Vec<TreeDiffItem>,
+    pub created_dirs: Vec<TreeDiffItem>,
+    pub changed: Vec<TreeDiffItem>,
+    pub deleted: Vec<TreeDiffItem>,
 }
 
+#[derive(Debug, PartialEq, Eq)]
 pub struct TreeDiffItem {
-    pub variant: TreeDiffItemVariant,
+    pub size: u64,
     pub path: PathBuf,
 }
 
-pub enum TreeDiffItemVariant {
-    Created,
-    Deleted,
-    Changed,
-    CreatedDir,
+impl TreeDiffItem {
+    fn from(path: PathBuf, size: u64) -> Self {
+        Self { size, path }
+    }
 }
 
 impl TreeDiff {
     pub fn cmp(old: &V1DirTreeNode, new: &V1DirTreeNode) -> Self {
+        let (created, created_dirs, changed) =
+            cmp_updated_recurse(old, new, PathBuf::new().as_path());
         Self {
-            deletes: cmp_on_recurse(old, new, PathBuf::new().as_path()),
+            created,
+            created_dirs,
+            changed,
+            deleted: cmp_del_recurse(old, new, PathBuf::new().as_path()),
         }
     }
 }
 
+impl From<&str> for TreeDiffItem {
+    fn from(value: &str) -> Self {
+        PathBuf::from(value).into()
+    }
+}
+
+impl From<PathBuf> for TreeDiffItem {
+    fn from(value: PathBuf) -> Self {
+        Self::from(value, 0)
+    }
+}
+
 // compare old to new
-fn cmp_on_recurse(old: &V1DirTreeNode, new: &V1DirTreeNode, current: &Path) -> Vec<PathBuf> {
+fn cmp_del_recurse(old: &V1DirTreeNode, new: &V1DirTreeNode, current: &Path) -> Vec<TreeDiffItem> {
     let mut items = Vec::new();
 
     match &old.content {
@@ -41,18 +65,32 @@ fn cmp_on_recurse(old: &V1DirTreeNode, new: &V1DirTreeNode, current: &Path) -> V
                     let find = match find {
                         Some(f) => f,
                         None => {
-                            items.push(current.join(&entry.name));
+                            items.push(TreeDiffItem::from(
+                                current.join(&entry.name),
+                                if dir {
+                                    DIR_SIZE
+                                } else {
+                                    file_meta(&entry.content).1
+                                },
+                            ));
                             continue;
                         }
                     };
 
                     if dir != is_dir(&find.content) {
-                        items.push(current.join(&entry.name));
+                        items.push(TreeDiffItem::from(
+                            current.join(&entry.name),
+                            if dir {
+                                DIR_SIZE
+                            } else {
+                                file_meta(&entry.content).1
+                            },
+                        ));
                         continue;
                     }
 
                     if dir {
-                        items.append(&mut cmp_on_recurse(
+                        items.append(&mut cmp_del_recurse(
                             entry,
                             find,
                             current.join(&entry.name).as_path(),
@@ -60,7 +98,9 @@ fn cmp_on_recurse(old: &V1DirTreeNode, new: &V1DirTreeNode, current: &Path) -> V
                     }
                 }
             }
-            V1DirTreeItem::File { .. } => items.push(current.to_path_buf()),
+            V1DirTreeItem::File { size, .. } => {
+                items.push(TreeDiffItem::from(current.to_path_buf(), *size))
+            }
         },
         V1DirTreeItem::File { .. } => unreachable!(),
     }
@@ -68,6 +108,104 @@ fn cmp_on_recurse(old: &V1DirTreeNode, new: &V1DirTreeNode, current: &Path) -> V
     items
 }
 
+// compare new to old
+// (created, created_dirs , changed)
+fn cmp_updated_recurse(
+    old: &V1DirTreeNode,
+    new: &V1DirTreeNode,
+    current: &Path,
+) -> (Vec<TreeDiffItem>, Vec<TreeDiffItem>, Vec<TreeDiffItem>) {
+    let mut created = Vec::new();
+    let mut created_dirs = Vec::new();
+    let mut changed = Vec::new();
+
+    match &new.content {
+        V1DirTreeItem::Dir { content: nc } => match &old.content {
+            V1DirTreeItem::Dir { content: oc } => {
+                for entry in nc.iter() {
+                    let dir = is_dir(&entry.content);
+                    let find = oc.iter().find(|old_entry| old_entry.name == entry.name);
+
+                    let find = match find {
+                        Some(f) => f,
+                        None if dir => {
+                            created_dirs
+                                .push(TreeDiffItem::from(current.join(&entry.name), DIR_SIZE));
+
+                            let (mut sub_created, mut sub_created_dirs, mut sub_changed) =
+                                cmp_updated_recurse(
+                                    &V1DirTreeNode {
+                                        visibility: DEFAULT_VIS,
+                                        name: String::new(),
+                                        content: V1DirTreeItem::Dir {
+                                            content: Vec::new(),
+                                        },
+                                    },
+                                    entry,
+                                    current.join(&entry.name).as_path(),
+                                );
+                            created.append(&mut sub_created);
+                            created_dirs.append(&mut sub_created_dirs);
+                            changed.append(&mut sub_changed);
+                            continue;
+                        }
+                        None => {
+                            created.push(TreeDiffItem::from(
+                                current.join(&entry.name),
+                                file_meta(&entry.content).1,
+                            ));
+                            continue;
+                        }
+                    };
+
+                    if dir != is_dir(&find.content) {
+                        if dir {
+                            created_dirs
+                                .push(TreeDiffItem::from(current.join(&entry.name), DIR_SIZE));
+                        } else {
+                            created.push(TreeDiffItem::from(
+                                current.join(&entry.name),
+                                file_meta(&entry.content).1,
+                            ));
+                        }
+                        continue;
+                    }
+
+                    if dir {
+                        let (mut sub_created, mut sub_created_dirs, mut sub_changed) =
+                            cmp_updated_recurse(find, entry, current.join(&entry.name).as_path());
+                        created.append(&mut sub_created);
+                        created_dirs.append(&mut sub_created_dirs);
+                        changed.append(&mut sub_changed);
+                        continue;
+                    }
+
+                    let (last_modified, size) = file_meta(&entry.content);
+                    if last_modified > file_meta(&find.content).0 {
+                        changed.push(TreeDiffItem::from(current.join(&entry.name), size))
+                    }
+                }
+            }
+            V1DirTreeItem::File { size, .. } => {
+                created.push(TreeDiffItem::from(current.to_path_buf(), *size))
+            }
+        },
+        V1DirTreeItem::File { .. } => unreachable!(),
+    }
+
+    (created, created_dirs, changed)
+}
+
 fn is_dir(item: &V1DirTreeItem) -> bool {
     matches!(item, V1DirTreeItem::Dir { .. })
+}
+
+fn file_meta(item: &V1DirTreeItem) -> (u64, u64) {
+    match item {
+        V1DirTreeItem::File {
+            last_modified,
+            size,
+        } => (*last_modified, *size),
+        V1DirTreeItem::Dir { .. } => unreachable!("expected file only"),
+    }
 }
--- src/tests/diff.rs
+++ src/tests/diff.rs
@@ -1,6 +1,5 @@
 use crate::{functions::DEFAULT_VIS, structs::TreeDiff};
 use goodmorning_bindings::services::v1::{V1DirTreeItem, V1DirTreeNode};
-use std::path::PathBuf;
 
 #[test]
 fn delete_1() {
@@ -24,9 +23,9 @@ fn delete_1() {
         content: V1DirTreeItem::Dir { content: vec![] },
     };
 
-    let diff = TreeDiff::cmp(&tree1, &tree2).deletes;
+    let diff = TreeDiff::cmp(&tree1, &tree2).deleted;
 
-    assert_eq!(diff, vec![PathBuf::from("hi")])
+    assert_eq!(diff, vec!["hi".into()])
 }
 
 #[test]
@@ -78,9 +77,9 @@ fn delete_2() {
         },
     };
 
-    let diff = TreeDiff::cmp(&tree1, &tree2).deletes;
+    let diff = TreeDiff::cmp(&tree1, &tree2).deleted;
 
-    assert_eq!(diff, vec![PathBuf::from("test1"), PathBuf::from("test3")])
+    assert_eq!(diff, vec!["test1".into(), "test3".into()])
 }
 
 #[test]
@@ -160,14 +159,186 @@ fn delete_3() {
         },
     };
 
-    let diff = TreeDiff::cmp(&tree1, &tree2).deletes;
+    let diff = TreeDiff::cmp(&tree1, &tree2).deleted;
 
     assert_eq!(
         diff,
         vec![
-            PathBuf::from("test1"),
-            PathBuf::from("test2/test3"),
-            PathBuf::from("test4/test5/test6")
+            "test1".into(),
+            "test2/test3".into(),
+            "test4/test5/test6".into()
         ]
+    )
+}
+
+#[test]
+fn all_1() {
+    let tree1 = V1DirTreeNode {
+        name: "hello".to_string(),
+        visibility: DEFAULT_VIS,
+        content: V1DirTreeItem::Dir {
+            content: vec![
+                V1DirTreeNode {
+                    name: "test1".to_string(),
+                    visibility: DEFAULT_VIS,
+                    content: V1DirTreeItem::File {
+                        last_modified: 0,
+                        size: 0,
+                    },
+                },
+                V1DirTreeNode {
+                    name: "test2".to_string(),
+                    visibility: DEFAULT_VIS,
+                    content: V1DirTreeItem::Dir {
+                        content: vec![V1DirTreeNode {
+                            name: "test3".to_string(),
+                            visibility: DEFAULT_VIS,
+                            content: V1DirTreeItem::File {
+                                last_modified: 0,
+                                size: 0,
+                            },
+                        }],
+                    },
+                },
+                V1DirTreeNode {
+                    name: "test4".to_string(),
+                    visibility: DEFAULT_VIS,
+                    content: V1DirTreeItem::Dir {
+                        content: vec![V1DirTreeNode {
+                            name: "test5".to_string(),
+                            visibility: DEFAULT_VIS,
+                            content: V1DirTreeItem::Dir {
+                                content: vec![V1DirTreeNode {
+                                    name: "test6".to_string(),
+                                    visibility: DEFAULT_VIS,
+                                    content: V1DirTreeItem::File {
+                                        last_modified: 0,
+                                        size: 0,
+                                    },
+                                }],
+                            },
+                        }],
+                    },
+                },
+            ],
+        },
+    };
+    let tree2 = V1DirTreeNode {
+        name: "hello".to_string(),
+        visibility: DEFAULT_VIS,
+        content: V1DirTreeItem::Dir {
+            content: vec![
+                V1DirTreeNode {
+                    name: "test2".to_string(),
+                    visibility: DEFAULT_VIS,
+                    content: V1DirTreeItem::Dir { content: vec![] },
+                },
+                V1DirTreeNode {
+                    name: "test4".to_string(),
+                    visibility: DEFAULT_VIS,
+                    content: V1DirTreeItem::Dir {
+                        content: vec![V1DirTreeNode {
+                            name: "test5".to_string(),
+                            visibility: DEFAULT_VIS,
+                            content: V1DirTreeItem::Dir { content: vec![] },
+                        }],
+                    },
+                },
+                V1DirTreeNode {
+                    name: "test1".to_string(),
+                    visibility: DEFAULT_VIS,
+                    content: V1DirTreeItem::File {
+                        last_modified: 1000,
+                        size: 0,
+                    },
+                },
+            ],
+        },
+    };
+
+    let diff = TreeDiff::cmp(&tree1, &tree2);
+
+    assert_eq!(
+        diff,
+        TreeDiff {
+            deleted: vec!["test2/test3".into(), "test4/test5/test6".into()],
+            changed: vec!["test1".into(),],
+            ..Default::default()
+        }
+    )
+}
+
+#[test]
+fn all_2() {
+    let tree1 = V1DirTreeNode {
+        name: "hello".to_string(),
+        visibility: DEFAULT_VIS,
+        content: V1DirTreeItem::Dir {
+            content: vec![
+                V1DirTreeNode {
+                    name: "test1".to_string(),
+                    visibility: DEFAULT_VIS,
+                    content: V1DirTreeItem::Dir { content: vec![] },
+                },
+                V1DirTreeNode {
+                    name: "test4".to_string(),
+                    visibility: DEFAULT_VIS,
+                    content: V1DirTreeItem::File {
+                        last_modified: 0,
+                        size: 0,
+                    },
+                },
+            ],
+        },
+    };
+    let tree2 = V1DirTreeNode {
+        name: "hello".to_string(),
+        visibility: DEFAULT_VIS,
+        content: V1DirTreeItem::Dir {
+            content: vec![
+                V1DirTreeNode {
+                    name: "test1".to_string(),
+                    visibility: DEFAULT_VIS,
+                    content: V1DirTreeItem::File {
+                        last_modified: 0,
+                        size: 0,
+                    },
+                },
+                V1DirTreeNode {
+                    name: "test2".to_string(),
+                    visibility: DEFAULT_VIS,
+                    content: V1DirTreeItem::Dir {
+                        content: vec![V1DirTreeNode {
+                            name: "test3".to_string(),
+                            visibility: DEFAULT_VIS,
+                            content: V1DirTreeItem::File {
+                                last_modified: 0,
+                                size: 0,
+                            },
+                        }],
+                    },
+                },
+                V1DirTreeNode {
+                    name: "test4".to_string(),
+                    visibility: DEFAULT_VIS,
+                    content: V1DirTreeItem::File {
+                        last_modified: 1,
+                        size: 0,
+                    },
+                },
+            ],
+        },
+    };
+
+    let diff = TreeDiff::cmp(&tree1, &tree2);
+
+    assert_eq!(
+        diff,
+        TreeDiff {
+            created: vec!["test1".into(), "test2/test3".into()],
+            created_dirs: vec!["test2".into()],
+            deleted: vec!["test1".into()],
+            changed: vec!["test4".into()],
+        }
     )
 }
